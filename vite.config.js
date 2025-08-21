da90d0bc1528d012c1a8751c113fc5719f54009c
prakashydv01 <135949364+prakashydv01@users.noreply.github.com>
2025-08-21 13:57:22 +0545

    vercel file added

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/", // 👈 important for correct routing on Vercel
+  build: {
+    outDir: "dist", // default, but good to keep explicit
+  },
+  server: {
+    port: 5173, // optional, default vite dev server port
+  },
 })
